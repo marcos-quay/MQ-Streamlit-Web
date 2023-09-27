41f9dcb653adec20e0b0592e106459c1b24ac999
AryaStark13 <arihant.sheth0802@gmail.com>
2023-09-27 16:00:57 +0530

    updated firebase backend

--- main.py
+++ main.py
@@ -18,24 +18,25 @@ if not firebase_admin._apps:
 
 db = firestore.client()
 storage_client = storage.Client(project='mq-video-app')
+bucket_name = 'mq-videos'
+collection_name = 'video-assign'
 
 # Function to generate names of videos and categories
 
 
-def get_category_video_names(collection_name):
-    docs = db.collection(collection_name).get()
+def get_category_video_names():
+    blobs = storage_client.list_blobs(bucket_name)
     videos = {}
+    for blob in blobs:
+        video_url = f"https://storage.googleapis.com/{bucket_name}/{blob.name}"
 
-    for doc in docs:
-        url = doc.to_dict()['URL']
-        category = url.split('/')[-2]
-        videos[category] = []
+        sport = blob.name.split('/')[1]
+        video_name = blob.name.split('/')[-1].split('.')[0]
 
-    for doc in docs:
-        url = doc.to_dict()['URL']
-        category = url.split('/')[-2]
-        video_name = url.split('/')[-1].split('.')[0]
-        videos[category].append(video_name)
+        if videos.get(sport) is None:
+            videos[sport] = []
+
+        videos[sport].append(video_name)
 
     videos = dict(sorted(videos.items()))
     videos = {k: sorted(v) for k, v in videos.items()}
@@ -72,68 +73,40 @@ def get_coaches_names():
 # Function to update the "coaches" field of a video document
 
 
-def update_coaches(video_name, coaches):
-    if type(video_name) == list:
-        for video in video_name:
-            doc_ref = db.collection("videos").document(video)
-            doc_ref.update({"coaches": coaches})
-    else:
-        doc_ref = db.collection("videos").document(video_name)
-        doc_ref.update({"coaches": coaches})
+def update_coaches(selected_sport, video_names, coaches):
 
-# write a function to reset the coaches field of all the documents in the collection
+    for idx, coach in enumerate(coaches):
+        doc_ref = db.collection(collection_name).document(coach)
+        doc_ref.update({
+            'videos_assigned': firestore.ArrayUnion([f'https://storage.googleapis.com/{bucket_name}/MQ-Videos-All/{selected_sport}/{video_name}.mp4' for video_name in video_names])
+        })
 
 
 def reset_coaches():
-    docs = db.collection("videos").get()
+    docs = db.collection(collection_name).get()
     for doc in docs:
-        doc.reference.update({"coaches": []})
+        doc.reference.update({"videos_assigned": []})
 
 
 def create_pie_chart():
-    docs = db.collection("videos").get()
+    docs = db.collection(collection_name).get()
     pie_chart_data = {}  # {video_name: coaches_count}
 
     for doc in docs:
         doc_dict = doc.to_dict()
-        if doc_dict['coaches']:
-            url = doc_dict['URL']
-            video_name = url.split('/')[-1].split('.')[0]
-            pie_chart_data[video_name] = len(doc_dict['coaches'])
-
+        if doc_dict['videos_assigned']:
+            for video in doc_dict['videos_assigned']:
+                video_name = video.split('/')[-1].split('.')[0]
+                if pie_chart_data.get(video_name) is None:
+                    pie_chart_data[video_name] = 0
+                pie_chart_data[video_name] += 1
+    
     fig = px.pie(values=list(pie_chart_data.values()),
                  names=list(pie_chart_data.keys()))
     fig.update_traces(textposition='inside', textinfo='percent+label')
     fig.update_layout(uniformtext_minsize=16, uniformtext_mode='hide')
 
     return fig
-
-
-def add_to_firebase(blobs, bucket_name="mq-videos", collection_name="videos"):
-    count_new = 0
-    count_old = 0
-
-    for blob in blobs:
-        video_url = f"https://storage.googleapis.com/{bucket_name}/{blob.name}"
-        video_name = blob.name.split('/')[-1].split('.')[0]
-
-        # Add the video to Firebase if it doesn't already exist
-        doc_ref = db.collection(collection_name).document(video_name)
-        doc = doc_ref.get()
-
-        if not doc.exists:
-            doc_ref.set({
-                "URL": video_url,
-                "coaches": []
-            })
-            count_new += 1
-
-            st.write(f"Added video {video_name} to Firebase collection")
-        else:
-            count_old += 1
-
-    st.write(f"Added {count_new} new videos to Firebase collection")
-    st.write(f"{count_old} videos already existed in Firebase collection")
 
 
 def generate_password(user_name):
@@ -199,11 +172,11 @@ def main():
         "Random Group 3": all_coaches[2*len(all_coaches)//3:]
     }
 
-    videos = get_category_video_names("videos")
+    videos = get_category_video_names()
 
     with st.sidebar:
         selected = st.radio("Select Option", [
-                            "Assign Videos", "Add New Videos", "Add New Coaches", "Delete Coaches"])
+                            "Assign Videos", "Add New Coaches", "Delete Coaches", "Add New Videos"])
 
     if selected == "Assign Videos":
         st.markdown(
@@ -240,8 +213,9 @@ def main():
 
         # Update button
         if st.button("Update Coaches"):
-            if video_name and edited_coaches_emails:
-                update_coaches(video_name, edited_coaches_emails)
+            if video_name and edited_coaches_emails and selected_sport:
+                update_coaches(selected_sport, video_name,
+                               edited_coaches_emails)
                 st.success(
                     f"Coaches for video '{video_name}' updated successfully!")
             else:
@@ -255,35 +229,6 @@ def main():
         st.header("Video - Coaches Distribution")
         fig = create_pie_chart()
         st.plotly_chart(fig, use_container_width=True)
-
-    elif selected == "Add New Videos":
-        st.markdown("""Upload new videos to Google Cloud: [Upload Videos](https://console.cloud.google.com/storage/browser/mq-videos/MQ-Videos-All)
-
-**Note:**
-1. Videos cannot have the same name
-2. Upload videos to the correct folder
-3. If folder doesn't exist, create a new folder with the name of the sport
-4. Once the videos are uploaded, click on the "Add to Firebase" button below. \
-This will add the videos to the Firebase collection. \
-Without this step, the videos won't be visible in the app.
-                    
-This will only add new videos, old videos will not be affected.
-
-To check if the videos have been added to Firebase, go to the \
-[Firebase console](https://console.firebase.google.com/u/0/project/mq-video-app/firestore/data/~2Fvideos~2F) \
-and check if the videos are visible there. 
-
-All access is given to Raahil's email (raahil@marcosquay.com)                
-
-For any issues, contact Arihant: [Call](tel:+918433567777) | [WhatsApp](https://wa.me/918433567777) 
-""")
-        bucket_name = 'mq-videos'
-        bucket = storage_client.bucket(bucket_name)
-        collection_name = 'videos'
-        blobs = storage_client.list_blobs(bucket_name)
-
-        if st.button("Add to Firebase"):
-            add_to_firebase(blobs, bucket_name, collection_name)
 
     elif selected == "Add New Coaches":
         existing_coaches_df = pd.read_excel("coach_details.xlsx")
@@ -383,6 +328,19 @@ Make sure to save this file. This file will not be available after you close the
             all_coaches, emails = get_coaches_names()
             all_email_name_dict = dict(zip(emails, all_coaches))
             all_name_email_dict = dict(zip(all_coaches, emails))
+
+    elif selected == "Add New Videos":
+        st.markdown("""Upload new videos to Google Cloud: [Upload Videos](https://console.cloud.google.com/storage/browser/mq-videos/MQ-Videos-All)
+
+**Note:**
+1. Videos cannot have the same name
+2. Upload videos to the correct folder
+3. If folder doesn't exist, create a new folder with the name of the sport
+
+All access is given to Raahil's email (raahil@marcosquay.com)                
+
+For any issues, contact Arihant: [Call](tel:+918433567777) | [WhatsApp](https://wa.me/918433567777) 
+""")
 
 
 if __name__ == "__main__":
